c56506467515d266fd4c4e318547d9bcc276b434
oscar <oscarprietofernandez@gmail.com>
2017-11-27 13:23:22 +0100

    TodoController

---
+++ ToDoApi/Controllers/TodoController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi;
+using ToDoApi.Model;
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+        private object id;
+        private object item;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.Items.Count() == 0)
+            {
+                _context.Items.Add(new TodoItem
+                {
+                    Name = "Estudiar en casa"
+                });
+
+                _context.Items.Add(new TodoItem
+                {
+                    Name = "Hacer ejercicios en casa"
+                });
+                _context.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IEnumerable<TodoItem> get()
+        {
+            return _context.Items;
+        }
+
+        [HttpGet("{id}", Name = "GetTodoItem")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var item = await _context.Items.SingleOrDefaultAsync(x => x.Id == id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] TodoItem item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtRoute("GetTodoItem", new { id = item.Id }, item);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var item = await _context.Items.SingleOrDefaultAsync(x => x.Id == id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] TodoItem item)
+        {
+            if (!ModelState.IsValid || id != item.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_context.Items.Any(x => x.Id == id))
+                {
+                    return NotFound();
+                }
+
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+    }
+}
--- ToDoApi/Controllers/ValuesController.cs
+++ ToDoApi/Controllers/ValuesController.cs
@@ -32,11 +32,11 @@ namespace ToDoApi.Controllers
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
-        {
-        }
+       {
+       }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
+       // DELETE api/values/5
+       [HttpDelete("{id}")]
         public void Delete(int id)
         {
         }
